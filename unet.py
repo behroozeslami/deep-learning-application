f6cc3f12b1855f39fad80277d5d9cc36b3b7ca0d
beslami <behrouz.eslamimossallam@tno.nl>
2023-03-18 15:16:22 +0100

    init_model_params was added.

--- unet.py
+++ unet.py
@@ -81,3 +81,14 @@ class Unet(nn.Module):
         out = self.conv1(out)
         
         return out
+    
+    def init_model_params(self):
+            
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
